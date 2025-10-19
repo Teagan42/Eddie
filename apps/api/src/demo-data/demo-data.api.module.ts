f9c222378416c42ec836ab111765af2e99fdbbdf
Teagan Glenn <that@teagantotally.rocks>
2025-10-19 13:15:47 -0600

    Revert "Add demo fixture validation for API demo data module (#644)"
    This reverts commit 61a2db9e888f2dbe768517653606a9d0d6d9578f.

--- apps/api/src/demo-data/demo-data.api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@eddie/config";
-import { DemoDataLoader } from "./demo-data.loader";
-
-@Module({
-  imports: [ConfigModule],
-  providers: [DemoDataLoader],
-})
-export class DemoDataApiModule {}
