cc7b0c6dcba3ebe76b7dbe5e6f4fde517e3c3717
Teagan Glenn <that@teagantotally.rocks>
2025-10-19 12:45:27 -0600

    refactor(ui): expose readonly metadata alias

--- tests/helpers/fs.ts
+++ tests/helpers/fs.ts
@@ -5,3 +5,8 @@ import { fileURLToPath } from 'node:url';
 const repoRoot = fileURLToPath(new URL('../..', import.meta.url));
 
 export const read = (relative: string) => readFileSync(join(repoRoot, relative), 'utf8');
+
+export const readJson = <T>(relative: string): T => {
+  const contents = read(relative);
+  return JSON.parse(contents) as T;
+};
