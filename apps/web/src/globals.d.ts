122db7e690cb50d3e1ceeb350d0160fc763d6ffd
Teagan glenn <that@teagantotally.rocks>
2025-10-21 22:10:54 -0600

    Fix linting

--- apps/web/src/globals.d.ts
+++ apps/web/src/globals.d.ts
@@ -1,27 +1,13 @@
-import { Callout } from '@radix-ui/themes'
-import { PropsWithChildren, SelectHTMLAttributes } from 'react'
+import { SelectHTMLAttributes } from 'react';
 
-declare module '@radix-ui/react-tabs' {
-  export interface TabsProps extends PropsWithChildren { }
-  export interface TabsListProps extends PropsWithChildren { }
-  export interface TabsTriggerProps extends PropsWithChildren { }
-  export interface TabsContentProps extends PropsWithChildren { }
+interface WithChild {
+  asChild?: boolean;
+  children?: ReactNode | undefined;
 }
 
 declare module '@radix-ui/themes' {
-  export module DropDownMenu {
-    export interface DropdownMenuProps extends PropsWithChildren { }
-    export interface DropdownMenuItemProps extends PropsWithChildren { }
-  }
-  export module Callout {
-    export interface CalloutProps extends PropsWithChildren { }
-  }
-  export module Text {
-    export interface SelectTriggerProps extends PropsWithChildren { }
-  }
-  export module Select {
-    export interface SelectTriggerProps extends PropsWithChildren, SelectHTMLAttributes { }
-  }
-  export interface SelectProps extends PropsWithChildren, SelectHTMLAttributes { }
-  export interface SelectTriggerProps extends PropsWithChildren, SelectHTMLAttributes { }
+  export interface SelectProps extends WithChild, SelectHTMLAttributes { }
+  export interface SelectTriggerProps extends WithChild, SelectHTMLAttributes { };
+
+  export interface DropdownMenuTriggerProps extends WithChild, SelectHTMLAttributes { };
 }
--- platform/ui/src/globals.d.ts
+++ platform/ui/src/globals.d.ts
@@ -1,35 +1,13 @@
-// src/globals.d.ts (make sure tsconfig include covers this)
-import type { PropsWithChildren } from 'react';
+import { SelectHTMLAttributes } from 'react';
 
-// Augment React the right way
-declare module 'react' {
-  // Do not try to extend type aliases. Add the prop where you actually need it instead.
-  // If you insist on a helper, keep it local:
-  // type WithAsChild<T> = T & { asChild?: boolean };
+interface WithChild {
+  asChild?: boolean;
+  children?: ReactNode | undefined;
 }
 
-// Augment Radix tabs (verify the actual interface/type names in the package!)
-declare module '@radix-ui/react-tabs' {
-  // Example: if Radix exports these exact interfaces
-  interface TabsProps { asChild?: boolean }
-  interface TabsListProps { asChild?: boolean }
-  interface TabsTriggerProps { asChild?: boolean }
-  interface TabsContentProps { asChild?: boolean }
-}
-
-// Augment Radix Themes (names here MUST match reality)
 declare module '@radix-ui/themes' {
-  namespace DropdownMenu {
-    interface DropdownMenuRootProps { asChild?: boolean }
-    interface DropdownMenuItemProps { asChild?: boolean }
-    interface DropdownMenuTriggerProps { asChild?: boolean }
-  }
-  namespace Callout {
-    interface RootProps { asChild?: boolean }
-  }
-  namespace Select {
-    interface SelectRootProps { asChild?: boolean }
-    interface SelectItemProps { asChild?: boolean }
-    interface SelectTriggerProps { asChild?: boolean }
-  }
+  export interface SelectProps extends WithChild, SelectHTMLAttributes { }
+  export interface SelectTriggerProps extends WithChild, SelectHTMLAttributes { };
+
+  export interface DropdownMenuTriggerProps extends WithChild, SelectHTMLAttributes { };
 }
