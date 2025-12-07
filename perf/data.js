c456f2d6ded6ddce768ecd9ddc5ea257b2093e40
github-action-benchmark <github@users.noreply.github.com>
2025-12-07 00:19:25 +0000

    add Eddie performance benchmarks (customSmallerIsBetter) benchmark result for 6657f8bb41ea0bbde428a2bdc3ead5bea2ab98fb

--- perf/data.js
+++ perf/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764571157493,
+  "lastUpdate": 1765066765312,
   "repoUrl": "https://github.com/Teagan42/Eddie",
   "entries": {
     "Eddie performance benchmarks": [
@@ -3302,6 +3302,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "OpenAIAdapter.stream recorded scenarios › Simple completion (cold + warm)",
             "value": 23.61,
+            "unit": "ms",
+            "extra": "[object Object]"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "that@teagantotally.rocks",
+            "name": "Teagan glenn",
+            "username": "Teagan42"
+          },
+          "committer": {
+            "email": "that@teagantotally.rocks",
+            "name": "Teagan glenn",
+            "username": "Teagan42"
+          },
+          "distinct": true,
+          "id": "6657f8bb41ea0bbde428a2bdc3ead5bea2ab98fb",
+          "message": "updates",
+          "timestamp": "2025-12-06T17:08:53-07:00",
+          "tree_id": "ae05b7647633e79e2cca96208263de2a9f689062",
+          "url": "https://github.com/Teagan42/Eddie/commit/6657f8bb41ea0bbde428a2bdc3ead5bea2ab98fb"
+        },
+        "date": 1765066764620,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "OpenAIAdapter.stream recorded scenarios › Heavy tool-call loop (cold + warm)",
+            "value": 47.399,
+            "unit": "ms",
+            "extra": "[object Object]"
+          },
+          {
+            "name": "OpenAIAdapter.stream recorded scenarios › Mixed notifications (cold + warm)",
+            "value": 32.021,
+            "unit": "ms",
+            "extra": "[object Object]"
+          },
+          {
+            "name": "OpenAIAdapter.stream recorded scenarios › Simple completion (cold + warm)",
+            "value": 24.019,
             "unit": "ms",
             "extra": "[object Object]"
           }
